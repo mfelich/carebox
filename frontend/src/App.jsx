d07c9906600bf5727943308a9428c8e90693e4b5
mfelich <mfelich77@gmail.com>
2025-11-17 18:14:10 +0100

    Add frontend React + Vite + Tailwind setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+    <h1 className="text-3xl font-bold text-purple-400">CareBox frontend app</h1>
+    </>
+  )
+}
+
+export default App
